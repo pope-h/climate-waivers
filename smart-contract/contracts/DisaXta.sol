8129f6511fdc5cc79217b3b65e710758f42a0224
Ekarika Nsemeke <hamatthan@gmail.com>
2024-07-10 17:28:32 +0100

    redeloyed and verified with a new function added

--- smart-contract/contracts/DisaXta.sol
+++ smart-contract/contracts/DisaXta.sol
@@ -6,12 +6,21 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DisaXta is ERC20, Ownable {
     uint256 public constant INITIAL_SUPPLY = 1000000 * 10**18;
+    uint256 public constant CLAIMABLE_DSX = 100 * 10**18;
+
+    mapping(address => bool) public hasClaimed;
 
     constructor() ERC20("DisaXta", "DSX") Ownable(msg.sender) {
-        _mint(msg.sender, INITIAL_SUPPLY);
+        _mint(address(this), INITIAL_SUPPLY);
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function claimDSX() public {
+        require(!hasClaimed[msg.sender], "DSX token already claimed");
+        hasClaimed[msg.sender] = true;
+        _transfer(address(this), msg.sender, CLAIMABLE_DSX);
     }
 }
